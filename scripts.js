bef4d169e10cabfc069b1cf589761e2e0cbb2316
alamin6688 <alamin.kzs06@gmail.com>
2024-03-04 19:53:55 +0600

    category card loaded from API

--- scripts.js
+++ scripts.js
@@ -1,3 +1,68 @@
+const loadCategory = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
+    const data = await res.json();
+    const category = data.posts;
+    // console.log(category);
+    display(category);
+}
+
+const display = category =>{
+    // console.log(category)
+
+    const categoryContainer = document.getElementById('category-container');
+
+    category.forEach(item =>{
+        console.log(item);
+        // create a div
+        const categoryCard = document.createElement('div');
+        categoryCard.classList = `lg:flex justify-center items-center bg-gray-200 rounded-3xl p-3 shadow-2xl`;
+        // set innerHTML
+        categoryCard.innerHTML = `
+        <figure>
+            <div class="avatar online">
+                <div class="w-24 rounded-full">
+                  <img src="${item.image}"/>
+                </div>
+              </div>
+        </figure>
+        <div class="card-body">
+            <div class="lg:flex justify-start gap-6">
+                <div>
+                    <p>#<span> </span>${item.category}</p>
+                </div>
+                <div>
+                    <p>Author:<span> </span>${item.author.name}</p>
+                </div>
+            </div>
+            <h2 class="card-title">${item.title}</h2>
+            <p>${item.description}</p>
+            <div class="card-actions items-center justify-start">
+                <p><i class="fa-regular fa-comment-dots"></i>
+                    <span> </span>${item.comment_count}
+                </p>
+                <p><i class="fa-regular fa-eye"></i>
+                    <span> </span>${item.view_count}
+                </p>
+                <p><i class="fa-regular fa-clock"></i>
+                    <span> </span>${item.posted_time}
+                </p>
+                <p>
+                    <button class="btn bg-green-500 rounded-full">
+                        <i class="fa-solid fa-envelope-open text-white text-xl"></i>
+                    </button>
+                </p>
+            </div>
+        </div>
+        `;
+        // append child
+        categoryContainer.appendChild(categoryCard);
+    });
+}
+
+
+loadCategory();
+
+
 // const loadCategory = async(searchText) =>{
 //     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
 //     const data = await res.json();
